b8dbbfb552efcd34079a4c9c3ef184cf787a4e2b
zenite1996 <63078481+zenite1996@users.noreply.github.com>
2021-12-27 13:56:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from colorama import init, Fore, Back
 api_bot = "2118641728:AAG5uHqiYHEh3WRYc-gOtHSLOvAmGY4sh7U"
 bot = telebot.TeleBot(api_bot)
 g = Github(login_or_token="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")
-repo = g.get_user().get_repo('bot_iq')
+repo = g.get_user().get_repo('autoIQ')
 all_files = []
 contents = repo.get_contents("")
 content = str(contents)
